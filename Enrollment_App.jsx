0662c29648fe73834d3182e13d17a2da1676bc3e
Rithanya89 <rithanyamohanraj29@gmail.com>
2025-12-17 22:06:32 +0530

    Create Enrollment_App.jsx

---
+++ Enrollment_App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Enrollment from "./FullStack/React/Enrollment";
+
+
+export default function App() {
+  return (
+    <div>
+      <Enrollment />
+    </div>
+  );
+}
