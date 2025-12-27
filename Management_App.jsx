7f51fba2eb20da05f0eaf76a5ed29280b6bec825
Rithanya89 <rithanyamohanraj29@gmail.com>
2025-12-27 12:31:59 +0530

    Create Management_App.jsx

---
+++ Management_App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Management from "./FullStack/React/Management";
+
+
+
+export default function App() {
+  return (
+    <div>
+      <Management/>
+    </div>
+  );
+}
