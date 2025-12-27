4fe82aa5196d10eb50942ffecf973a79d45eaea5
Rithanya89 <rithanyamohanraj29@gmail.com>
2025-12-27 12:08:55 +0530

    Create Notification_App.jsx

---
+++ Notification_App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Notification from "./FullStack/React/Notification";
+
+export default function App() {
+  return (
+    <div>
+      <Notification/>
+    </div>
+  );
+}
