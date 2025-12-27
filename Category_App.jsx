b0dd1be7b8686a35742f80b93851e02f69979831
Rithanya89 <rithanyamohanraj29@gmail.com>
2025-12-27 12:36:01 +0530

    Create Category_App.jsx

---
+++ Category_App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Category from "./FullStack/React/Category";
+
+
+
+
+export default function App() {
+  return (
+    <div>
+      <Category/>
+    </div>
+  );
+}
